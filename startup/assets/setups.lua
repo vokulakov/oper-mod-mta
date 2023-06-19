8d5d10d18d6b9c6169f428620e49ea5cba0ae697
Vladimir Kulakov <vokulakov01@gmail.com>
2023-06-19 21:20:48 +0300

    Обновление версии мода до 1.1.0

--- startup/assets/setups.lua
+++ startup/assets/setups.lua
@@ -1,6 +1,6 @@
 MAP_NAME = "San-Andreas" -- карта
 GAME_TYPE = "OPER STYLE ORIGINAL" -- игровой режим
-MOD_INFO = "OPERSKIY × OTDEL 1.0.0" -- информация о моде
+MOD_INFO = "OPERSKIY × OTDEL 1.1.0" -- информация о моде
 SERVER_PASSWORD = "" -- пароль
 
 SHOW_MESSAGE = true -- показывать сообщение в чате перед выключением
