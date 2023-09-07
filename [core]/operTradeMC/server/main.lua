17a1f4157a02d506bbfaeea7db18d7653c85a911
Vladimir Kulakov <vokulakov01@gmail.com>
2023-09-08 00:44:54 +0300

    Автодонат. Инициализация системы.

---
+++ [core]/operTradeMC/server/main.lua
@@ -0,0 +1,10 @@
+Donate = {}
+
+-- Выводить сообщение в чат о том, что такой то игрок поддержал проект
+-- Так же проигрывать звук фанфар
+
+function Donate.addAccountPremium(acc_username)
+end
+
+-- exports
+addAccountPremium = Donate.addAccountPremium
