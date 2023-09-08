33e3d24de161a9013f1fdc8f81a1d340a0b24d29
Vladimir Kulakov <vokulakov@1872277-vokulakov.twc1.net>
2023-09-08 23:33:57 +0300

    Наброски системы выдачи привелегий

--- [core]/operTradeMC/server/main.lua
+++ [core]/operTradeMC/server/main.lua
@@ -3,8 +3,12 @@ Donate = {}
 -- Выводить сообщение в чат о том, что такой то игрок поддержал проект
 -- Так же проигрывать звук фанфар
 
-function Donate.addAccountPremium(acc_username)
+function Donate.addAccountPremium(acc_username, time_action)
+end
+
+function Donate.removeAccountPremium(acc_username)
 end
 
 -- exports
 addAccountPremium = Donate.addAccountPremium
+removeAccountPremium = Donate.removeAccountPremium
